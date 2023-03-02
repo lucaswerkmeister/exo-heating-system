05a4cfb57a0c7645217d1231b057c823c40109c0
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2023-03-02 17:39:31 +0100

    Extract sendToSocket() method
    Instead of duplicating all this code.

--- src/HeatingManagerImpl.php
+++ src/HeatingManagerImpl.php
@@ -3,25 +3,20 @@
 class HeatingManagerImpl {
 	public function manageHeating( float $temperature, float $threshold ): void {
 		if ( $temperature < $threshold ) {
-			if ( !( $socket = socket_create( AF_INET, SOCK_STREAM, 0 ) ) ) {
-				throw new RuntimeException( 'could not create socket' );
-			}
-			if ( !socket_connect( $socket, 'heater.home', 9999 ) ) {
-				throw new RuntimeException( 'could not connect!' );
-			}
-			$message = "on";
-			socket_send( $socket, $message, strlen( $message ), 0 );
-			socket_close( $socket );
+			$this->sendToSocket( 'on' );
 		} elseif ( $temperature > $threshold ) {
-			if ( !( $socket = socket_create( AF_INET, SOCK_STREAM, 0 ) ) ) {
-				throw new RuntimeException( 'could not create socket' );
-			}
-			if ( !socket_connect( $socket, 'heater.home', 9999 ) ) {
-				throw new RuntimeException( 'could not connect!' );
-			}
-			$message = "off";
-			socket_send( $socket, $message, strlen( $message ), 0 );
-			socket_close( $socket );
+			$this->sendToSocket( 'off' );
 		}
+	}
+
+	private function sendToSocket( string $message ): void {
+		if ( !( $socket = socket_create( AF_INET, SOCK_STREAM, 0 ) ) ) {
+			throw new RuntimeException( 'could not create socket' );
+		}
+		if ( !socket_connect( $socket, 'heater.home', 9999 ) ) {
+			throw new RuntimeException( 'could not connect!' );
+		}
+		socket_send( $socket, $message, strlen( $message ), 0 );
+		socket_close( $socket );
 	}
 }
