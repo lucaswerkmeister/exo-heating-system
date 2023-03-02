68fa703b37a4fa708ff08a792686c932f59bc62d
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2023-03-02 17:37:24 +0100

    Throw exceptions from manageHeating()
    Instead of catching any unknown exception, but also killing the whole
    PHP process under other conditions. The caller should be responsible for
    catching exceptions if it wants to.

--- src/HeatingManagerImpl.php
+++ src/HeatingManagerImpl.php
@@ -3,33 +3,25 @@
 class HeatingManagerImpl {
 	public function manageHeating( float $temperature, float $threshold ): void {
 		if ( $temperature < $threshold ) {
-			try {
-				if ( !( $socket = socket_create( AF_INET, SOCK_STREAM, 0 ) ) ) {
-					die( 'could not create socket' );
-				}
-				if ( !socket_connect( $socket, 'heater.home', 9999 ) ) {
-					die( 'could not connect!' );
-				}
-				$message = "on";
-				socket_send( $socket, $message, strlen( $message ), 0 );
-				socket_close( $socket );
-			} catch ( Exception $e ) {
-				echo 'Caught exception: ', $e->getMessage(), "\n";
+			if ( !( $socket = socket_create( AF_INET, SOCK_STREAM, 0 ) ) ) {
+				throw new RuntimeException( 'could not create socket' );
 			}
+			if ( !socket_connect( $socket, 'heater.home', 9999 ) ) {
+				throw new RuntimeException( 'could not connect!' );
+			}
+			$message = "on";
+			socket_send( $socket, $message, strlen( $message ), 0 );
+			socket_close( $socket );
 		} elseif ( $temperature > $threshold ) {
-			try {
-				if ( !( $socket = socket_create( AF_INET, SOCK_STREAM, 0 ) ) ) {
-					die( 'could not create socket' );
-				}
-				if ( !socket_connect( $socket, 'heater.home', 9999 ) ) {
-					die( 'could not connect!' );
-				}
-				$message = "off";
-				socket_send( $socket, $message, strlen( $message ), 0 );
-				socket_close( $socket );
-			} catch ( Exception $e ) {
-				echo 'Caught exception: ', $e->getMessage(), "\n";
+			if ( !( $socket = socket_create( AF_INET, SOCK_STREAM, 0 ) ) ) {
+				throw new RuntimeException( 'could not create socket' );
 			}
+			if ( !socket_connect( $socket, 'heater.home', 9999 ) ) {
+				throw new RuntimeException( 'could not connect!' );
+			}
+			$message = "off";
+			socket_send( $socket, $message, strlen( $message ), 0 );
+			socket_close( $socket );
 		}
 	}
 }
